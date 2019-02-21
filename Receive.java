4861848dea9c44087028f38a8ac1319209d51bb4
Tristan Russell <39279889+Tristan-R@users.noreply.github.com>
2019-02-21 22:33:12 +0000

    Add socket handelling.

--- Receive.java
+++ Receive.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.SocketException;
+import java.util.ArrayList;
 
-public class Receive {
+public class Receive implements Runnable {
+
+    BufferedReader in;
+    ArrayList<String> messages;
+
+    Receive(BufferedReader in, ArrayList<String> messages) {
+        this.in = in;
+        this.messages = messages;
+    }
+
+    public void run() {
+        try {
+            String input;
+
+            while(true) {
+                input = in.readLine();
+
+                messages.add(input);
+                notifyAll();
+
+            }
+        } catch (SocketException e) {
+            System.err.println("Connection error.");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        } finally {
+            // Send a close call to .....
+        }
+    }
+
+
 }
