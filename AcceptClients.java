a4d65b0cd869f713c2e3d855cd38394e07a1c255
Tristan Russell <39279889+Tristan-R@users.noreply.github.com>
2019-02-21 22:31:47 +0000

    Add previous version code.

--- AcceptClients.java
+++ AcceptClients.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
-public class AcceptClients {
+public class AcceptClients implements Runnable {
+
+    private ServerSocket in;
+
+    public AcceptClients(ServerSocket in) {
+        this.in = in;
+    }
+
+    public void run() {
+        try {
+            while (true) {
+                Socket s = in.accept();
+                System.out.println("Server accepted connection on " + in.getLocalPort() + " ; " + s.getPort());
+                MsgControl client = new MsgControl(s); // Need to change to a non-abstract class
+                Thread thread = new Thread(client);
+                thread.start();
+            }
+
+        } catch (
+                IOException e) {
+            e.printStackTrace();
+
+        } finally {
+            try {
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
