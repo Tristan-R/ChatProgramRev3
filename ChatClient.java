e77d89ff12515b2b5030f213d736a9abaf0555e6
Tristan Russell <39279889+Tristan-R@users.noreply.github.com>
2019-02-23 21:48:08 +0000

    Add initial list of commands.

--- ChatClient.java
+++ ChatClient.java
@@ -1,2 +1,21 @@
-public class ChatClient {
+public class ChatClient { // Need to send '/' commands first
+    /*
+    Sending Commands:
+        0 - EXIT
+        1 - Send to server
+        2 - Send to all
+        3 - Direct message
+        4 - See current clients
+        5 - kicked from chat?
+        6 -
+
+    Receiving Commands:
+        0 - EXIT
+        1 - Received from server // Should there be a received from server direct?
+        2 - Received global message
+        3 - Received direct message
+        4 - List of clients
+        5 - kick command?
+        6 -
+     */
 }
