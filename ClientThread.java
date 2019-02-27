e55699baa9282ca6d260458d7ed16b971a71450d
Tristan Russell <39279889+Tristan-R@users.noreply.github.com>
2019-02-27 00:24:14 +0000

    Add support for new methods and remove clients list showing server bug.

--- ClientThread.java
+++ ClientThread.java
@@ -8,11 +8,22 @@ public class ClientThread extends MsgControl {
     }
 
     @Override
+    boolean endThread() {
+        return socket.isClosed();
+    }
+
+    @Override
+    String convertMsg(String message) {
+        return message;
+    }
+
+    @Override
     void exit() {
         try {
             clients.remove(name);
             socket.close();
             clients.get("server").println("Client has disconnected on " + socket.getLocalPort() + " : " + socket.getPort());
+
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -24,6 +35,7 @@ public class ClientThread extends MsgControl {
             String returnMsg = msgBuilder(6, "server", "Weak connection to server, attempting to reconnect.");
             out.println(returnMsg);
             exit();
+
         } else {
             String returnMsg = msgBuilder(1, "server", "We could not process your last message.");
             out.println(returnMsg);
@@ -46,7 +58,7 @@ public class ClientThread extends MsgControl {
         String messageOut = msgBuilder(2, name, message);
 
         for (String client : clients.keySet()) {
-            if (!client.equals(name)) {
+            if (!client.equals(name) && !client.equals("server")) {
                 clients.get(client).println(messageOut);
             }
         }
@@ -72,10 +84,12 @@ public class ClientThread extends MsgControl {
         String list = "";
         int i = 1;
         for (String client : clients.keySet()) {
-            list = list.concat("\t" + client);
+            if (!client.equals("server")) {
+                list = list.concat("\t" + client);
 
-            if (++i % 4 == 0) {
-                list = list.concat("\n");
+                if (++i % 4 == 0) {
+                    list = list.concat("\n");
+                }
             }
         }
 
@@ -86,15 +100,30 @@ public class ClientThread extends MsgControl {
     void kick(String kickedBy, String toKick) {
         if (admins.contains(name)) {
             if (clients.containsKey(toKick)) {
-                clients.remove(toKick);
+                if (admins.contains(toKick)) {
+                    String messageOut = msgBuilder(1, "server", "Only the server can kick admins.");
+                    out.println(messageOut);
 
+                } else {
+                    clients.remove(toKick);
+                    removeClients.add(toKick);
+
+                }
             } else {
-                String messageOut = msgBuilder(1, "server", "User does not exist.");
+                String messageOut = msgBuilder(1, "server", "This user does not exist.");
                 out.println(messageOut);
             }
         } else {
             String messageOut = msgBuilder(1, "server", "You do not have permission to do that.");
             out.println(messageOut);
+        }
+    }
+
+    @Override
+    void unknownCommand(int identifier, String from, String message) {
+        switch (identifier) {
+            default:
+                brokenMsg();
         }
     }
 }
