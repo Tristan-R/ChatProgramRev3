4d558932d0d152f232f7cb778b4ce9fa198dd934
Tristan Russell <39279889+Tristan-R@users.noreply.github.com>
2019-03-04 21:36:35 +0000

    Sort comments at top.

--- ChatClient.java
+++ ChatClient.java
@@ -5,28 +5,23 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-public class ChatClient { // Need to send '/' commands first
+public class ChatClient {
     /*
     Sending Commands:
         0 - EXIT
-        1 - Send to server // Needs to be an authorisation on server end
+        1 - Send to server
         2 - Send to all
-        3 - Direct message // message will have format (3)~(fromUser)~(toUser)>(message)
+        3 - Direct message
         4 - See current clients
         5 - Kicked from chat
-        6 - Reconnect to server? // Does the server need to know it's a reconnect? Or just send EXIT?
-        7 -
 
-    Receiving Commands: // need one for name change
+    Receiving Commands:
         0 - EXIT
-        1 - Received from server // Direct messages from server should be handled server end
-                                 // and the server should put (direct) before the message.
+        1 - Received from server
         2 - Received global message
         3 - Received direct message
         4 - List of clients
         5 - Kick command
-        6 - Try reconnecting
-        7 -
      */
 
     private Socket socket;
