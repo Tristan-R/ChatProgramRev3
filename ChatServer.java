c975703a0d0c8917bcf2cf5c13a2706647f5796e
Tristan Russell <39279889+Tristan-R@users.noreply.github.com>
2019-03-05 13:51:02 +0000

    Add documentation comments.

--- ChatServer.java
+++ ChatServer.java
@@ -1,10 +1,26 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 
+/**
+ * <h1>Launcher for Chat Server</h1>
+ * This launcher will start the chat server and will allow clients to connect.
+ * <p>
+ * The server will can also commands entered on the command line.
+ * See README.txt for full instructions.
+ */
 public class ChatServer {
 
+    /**
+     * Stores the socket that the server is running on.
+     */
     private ServerSocket server;
 
+    /**
+     * Constructor.
+     *
+     * @param port
+     *      The port to start the server on.
+     */
     private ChatServer(int port) {
         try {
             server = new ServerSocket(port);
@@ -16,12 +32,23 @@ public class ChatServer {
         }
     }
 
+    /**
+     * Starts a new thread for accepting clients and a new thread for receiving
+     * user commands.
+     */
     private void begin() {
         new Thread(new AcceptClients(server)).start();
 
         new Thread(new ServerThread(server)).start();
     }
 
+    /**
+     * This is the main method that checks for a new port number if specified
+     * on the command line at launch.
+     *
+     * @param args
+     *      Used to change the port number if -csp flag is entered.
+     */
     public static void main(String[] args) {
         int portNumber = 14001;
         for (int i = 0; i < args.length; i++) {
