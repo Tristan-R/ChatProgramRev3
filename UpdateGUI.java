d4978ccb6d671a484727722f24786688f91b67dd
Tristan Russell <39279889+Tristan-R@users.noreply.github.com>
2019-03-10 22:09:08 +0000

    Add class implementation and documentation.

--- UpdateGUI.java
+++ UpdateGUI.java
@@ -1,2 +1,22 @@
-public class UpdateGUI {
+import javafx.application.Platform;
+
+/**
+ * UpdateGUI class
+ * <p>
+ * Used to update ClientGUI from a non-application thread.
+ */
+class UpdateGUI {
+
+    /**
+     * Updates the name label in the GUI.
+     *
+     * @param GUI
+     *      The GUI controller.
+     *
+     * @param name
+     *      The name to set for the label.
+     */
+    static void setName(ClientGUIController GUI, String name) {
+        Platform.runLater(() -> GUI.setName(name));
+    }
 }
