4056cf3058080c231644a9c78bbff2ad3e45c4c5
Tristan Russell <39279889+Tristan-R@users.noreply.github.com>
2019-03-10 22:33:45 +0000

    Add code to date.

--- ServerGUI.java
+++ ServerGUI.java
@@ -1,4 +1,41 @@
-package PACKAGE_NAME;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class ServerGUI {
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class ServerGUI extends Application {
+
+    private static ServerSocket server;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("ServerGUI.fxml"));
+        primaryStage.setTitle("Chat Server");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setOnCloseRequest(e -> shutdown());
+        primaryStage.show();
+    }
+
+    private void shutdown() {
+        try {
+            server.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            System.exit(0);
+        }
+    }
+
+    public static ServerSocket getServer() {
+        return server;
+    }
+
+    static void begin(ServerSocket serverSocket) {
+        server = serverSocket;
+        launch();
+    }
 }
--- ServerGUIController.java
+++ ServerGUIController.java
@@ -1,4 +1,68 @@
-package PACKAGE_NAME;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.io.*;
+import java.net.ServerSocket;
 
 public class ServerGUIController {
+    public Label portNumber;
+    public TextArea chatBox;
+    public TextArea infoBox;
+    public TextField inputBox;
+    private ServerThread serverThread;
+
+    public void initialize() {
+        Console chatConsole = new Console(chatBox);
+        PrintStream chatStream = new PrintStream(chatConsole, true);
+        PrintWriter out = new PrintWriter(chatStream, true);
+        chatBox.setEditable(false);
+
+        Console infoConsole = new Console(infoBox);
+        PrintStream infoStream = new PrintStream(infoConsole, true);
+        System.setOut(infoStream);
+        System.setErr(infoStream);
+        infoBox.setDisable(true);
+
+        ServerSocket server = ServerGUI.getServer();
+
+        serverThread = new ServerThread(server, out);
+
+        portNumber.setText(Integer.toString(server.getLocalPort()));
+    }
+
+    private void sendInput() {
+        String input = inputBox.getCharacters().toString();
+        inputBox.setText("");
+
+        if (serverThread.endThread()) {
+            serverThread.exit();
+
+        } else {
+            serverThread.processMsg(input);
+        }
+    }
+
+    public void onPress(ActionEvent actionEvent) {
+        sendInput();
+    }
+
+    public void onEnter(ActionEvent actionEvent) {
+        sendInput();
+    }
+
+    public static class Console extends OutputStream {
+
+        private TextArea output;
+
+        public Console(TextArea ta) {
+            this.output = ta;
+        }
+
+        @Override
+        public void write(int i) throws IOException {
+            output.appendText(String.valueOf((char) i));
+        }
+    }
 }
