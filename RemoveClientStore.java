1070211499ec8db742c2e73b15f75780f7af8f04
Tristan Russell <39279889+Tristan-R@users.noreply.github.com>
2019-03-10 17:45:10 +0000

    Add store class for removing clients.

--- RemoveClientStore.java
+++ RemoveClientStore.java
@@ -1,4 +1,46 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
-public class RemoveClientStore {
+class RemoveClientStore {
+
+    /**
+     * A list of clients that have been kicked before their sockets are closed.
+     */
+    private static ArrayList<String> removeClients;
+
+    /**
+     * Constructor.
+     */
+    RemoveClientStore() {
+        removeClients = new ArrayList<>();
+    }
+
+    /**
+     * Synchronised method for adding or removing a client from the list of
+     * removed clients.
+     *
+     * @param operation
+     *      Identifies whether a client is to be added or removed.
+     *
+     * @param name
+     *      The name of the client to be added or removed.
+     */
+    synchronized void kickModify(String operation, String name) {
+        if (operation.equals("Add")) {
+            removeClients.add(name);
+        } else if (operation.equals("Remove")) {
+            removeClients.remove(name);
+        }
+    }
+
+    /**
+     * Synchronised method to check if a client needs to be kicked.
+     * @param name
+     *      The client name to search for.
+     *
+     * @return
+     *      Whether the client is in the list of kicked clients.
+     */
+    synchronized boolean kickSearch(String name) {
+        return removeClients.contains(name);
+    }
 }
